6cbaed2608531bad12317a85966526849bf84af2
colkito <github@colkito.com>
2024-09-06 23:13:30 -0300

    update core setup with vpn

--- core/wireguard/down.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
-iptables -D FORWARD -i eth0 -o wg0 -j ACCEPT
-iptables -D FORWARD -i wg0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
--- core/wireguard/up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
-iptables -A FORWARD -i wg0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
